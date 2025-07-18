6e8161688893e5b69a028af8b1fbb86a7f3b639c
sainagavarapu13 <sainagavarapu13@gmail.com>
2025-07-18 09:42:47 +0530

    Update Queue.cpp
    Signed-off-by: sainagavarapu13 <sainagavarapu13@gmail.com>

--- Queue.cpp
+++ Queue.cpp
@@ -3,65 +3,21 @@
 #include "Queue.h"
 using namespace std;
 
-Queue::Queue(int cap) {
-    capacity = cap;
-    arr = new int[capacity];
-    front = 0;
-    rear = -1;
-    size = 0;
-}
+int main() {
+    Queue queue;
 
-Queue::~Queue() {
-    delete[] arr;
-}
+    queue.enqueue(10);
+    queue.enqueue(20);
+    queue.enqueue(30);
 
-bool Queue::isEmpty() {
-    return size == 0;
-}
+    queue.display();
 
-bool Queue::isFull() {
-    return size == capacity;
-}
+    cout << "Front element is: " << queue.peek() << endl;
+    cout << "Queue size: " << queue.getSize() << endl;
 
-void Queue::enqueue(int value) {
-    if (isFull()) {
-        cout << "Queue is full. Cannot enqueue.\n";
-        return;
-    }
-    rear = (rear + 1) % capacity;
-    arr[rear] = value;
-    size++;
-    cout << value << " enqueued.\n";
-}
+    cout << queue.dequeue() << " dequeued from queue\n";
+    cout << queue.dequeue() << " dequeued from queue\n";
 
-int Queue::dequeue() {
-    if (isEmpty()) {
-        cout << "Queue is empty. Cannot dequeue.\n";
-        return -1;
-    }
-    int val = arr[front];
-    front = (front + 1) % capacity;
-    size--;
-    cout << val << " dequeued.\n";
-    return val;
-}
-
-int Queue::peek() {
-    if (isEmpty()) {
-        cout << "Queue is empty.\n";
-        return -1;
-    }
-    return arr[front];
-}
-
-void Queue::display() {
-    if (isEmpty()) {
-        cout << "Queue is empty.\n";
-        return;
-    }
-    cout << "Queue contents: ";
-    for (int i = 0; i < size; i++) {
-        cout << arr[(front + i) % capacity] << " ";
-    }
-    cout << endl;
+    queue.display();
+retrun 0;
 }
