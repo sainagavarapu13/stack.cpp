793037a1cbd59555123755b1ccc5ad46f1c92629
sainagavarapu13 <sainagavarapu13@gmail.com>
2025-07-18 10:11:24 +0530

    Update Stack.cpp
    Signed-off-by: sainagavarapu13 <sainagavarapu13@gmail.com>

--- Stack.cpp
+++ Stack.cpp
@@ -14,16 +14,7 @@ int main() {
     cout << "Top element is: " << stack.peek() << endl;
     cout << "Stack size: " << stack.getSize() << endl;
 
-    cout << stack.pop() << " popped from stack\n";
-    cout << stack.pop() << " popped from stack\n";
-
-    stack.display();
-
-    cout << "Top element is: " << stack.peek() << endl;
-    cout << "Stack size: " << stack.getSize() << endl;
-
-    cout << stack.pop() << " popped from stack\n";
-    cout << stack.pop() << " popped from stack\n";  // Underflow
+    
 
     return 0;
 }
